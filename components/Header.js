01c4b64ce043a9ad76da17fff877028f1cfb142c
Jay Sierra <jsierra99@live.com>
2021-11-28 22:19:53 -0800

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+import logo from "../img/hulu-logo.png";
+import HeaderItem from './HeaderItem';
+import { BadgeCheckIcon, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon, UserIcon } from "@heroicons/react/outline";
+
+const Header = () => 
+{
+    return (
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+            <div className="flex flex-grow justify-evenly max-w-2xl">
+                <HeaderItem title="HOME" Icon={HomeIcon} />
+                <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+                <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+                <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+                <HeaderItem title="SEARCH" Icon={SearchIcon} />
+                <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+            </div>
+            <Image className="object-contain" src={logo} width={200} height={100} alt=""/>
+        </header>
+    )
+}
+
+export default Header
